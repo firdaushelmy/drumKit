985b1fbdbba2ec95aec6f6fe880f4673a5c11153
Firdaus Helmy <46524543+firdaushelmy@users.noreply.github.com>
2020-05-23 03:07:13 +0800

    added loop for drum event listener

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
-document.querySelector("button").addEventListener("click", handleClick);
 
+// add an event listener to add the drums available.
+// start with var the query selector with drums and attach length
+// do a for loop and all in the var so it will keep looping the total length
+// add in query selector to select drum index and add individual event listeners
 
-function handleClick() {
-  alert("I got clicked!")
+var numberOfDrumButtons = document.querySelectorAll(".drum").length;
+
+for (var i = 0; i < numberOfDrumButtons; i++) {
+
+  document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+    alert("I got clicked!")
+  });
 }
+
