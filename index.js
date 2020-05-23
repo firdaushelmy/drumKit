e34b58422daad7b46322832055f7a45b54388728
Firdaus Helmy <46524543+firdaushelmy@users.noreply.github.com>
2020-05-24 04:45:58 +0800

    added switch statement to connect picture and sound

--- index.js
+++ index.js
@@ -1,8 +1,3 @@
-function tom1() {
-  var audio = new Audio('sounds/tom-1.mp3');
-  audio.play();
-}
-
 // add an event listener to add the drums available.
 // start with var the query selector with drums and attach length
 // do a for loop and all in the var so it will keep looping the total length
@@ -11,5 +6,50 @@ function tom1() {
 var numberOfDrumButtons = document.querySelectorAll(".drum").length;
 
 for (var i = 0; i < numberOfDrumButtons; i++) {
-  document.querySelectorAll(".drum")[i].addEventListener("click", tom1);
+  document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+
+    var buttonInnerHTML = this.innerHTML
+
+    switch (buttonInnerHTML) {
+      case "w":
+        var tom1 = new Audio('sounds/tom-1.mp3');
+        tom1.play();
+        break;
+
+      case "a":
+        var tom2 = new Audio('sounds/tom-2.mp3');
+        tom2.play();
+        break;
+
+      case "s":
+        var tom3 = new Audio('sounds/tom-3.mp3');
+        tom3.play();
+        break;
+
+      case "d":
+        var tom4 = new Audio('sounds/tom-4.mp3');
+        tom4.play();
+        break;
+
+      case "j":
+        var snare = new Audio('sounds/snare.mp3');
+        snare.play();
+        break;
+
+      case "k":
+        var crash = new Audio('sounds/crash.mp3');
+        crash.play();
+        break;
+
+      case "l":
+        var kick = new Audio('sounds/kick-bass.mp3');
+        kick.play();
+        break;
+
+      default: console.log(innerHTML)
+        break;
+    }
+
+
+  });
 }
